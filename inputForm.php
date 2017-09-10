07b286ff2b6d2b3fb50d0a24cd860845ee5e98ee
Fabian Ferrari <ferrarif97@gmail.com>
2017-09-10 17:46:17 +0200

    Newsfeed added

---
+++ inputForm.php
@@ -0,0 +1,34 @@
+<?php
+
+	if(Input::exists()) {
+		$title = Input::get('title');
+		$text = Input::get('text');
+		$id = Session::get(Config::get('session/session_name'));
+
+		$db = DB::getInstance();
+
+		$user = new User();
+		if($user->find($id)) {
+			if($user->hasPermission(2)) {
+				$db->insert('news', array(
+					'title' => $title,
+					'text' => $text,
+					'author' => $id
+				));
+			}
+		}
+	}
+
+?>
+
+<br />
+<br />
+<form method="POST" action="#">
+	<div class="form-group">
+		<input type="text" class="form-control" name="title" id="title" placeholder="Titel" required>
+	</div>
+	<div class="form-group">
+		<textarea class="form-control" rows="5" placeholder="Text" id="text" name="text" required></textarea>
+	</div>
+	<input class="btn btn-primary" type="submit" name="submit" value="Absenden">
+</form>
